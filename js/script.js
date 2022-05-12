073b03684bf9868c045374548e40560e9d1e26c6
matlencar <rm94642@fiap.com.br>
2022-05-11 21:13:46 -0300

    Js Basics

---
+++ js/script.js
@@ -0,0 +1,16 @@
+    const btnOn = document.querySelector(".ButtonLiga")
+
+    btnOn.addEventListener('click', function(){
+        document.querySelector("img").src = "./img/pic_bulbon.gif"
+    })
+
+    const btnOff = document.querySelector(".ButtonDesliga")
+
+    btnOff.addEventListener('click', function(){
+        document.querySelector("img").src = "./img/pic_bulboff.gif"        
+    })
+
+
+    //NORMALIZE ESSE EXERCÍCIO
+    //TRANSFORME ESTAS LINHAS EM APENAS UMA FUNÇÃO!
+    
