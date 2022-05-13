dde8cba760365825c4e1090f8b20580855f6e7e8
luckgon <rm88349@fiap.com.br>
2022-05-12 21:13:04 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,26 +1,23 @@
 
 function LigaDesliga(){
-   
-    const btnOn = document.querySelector(".ButtonLiga")
+    const btnOnOff = document.querySelector(".ButtonLiga");
+    const imagem = document.querySelector('#lampada').src;
+    const on = document.querySelector("img").src = "./img/pic_bulbon.gif";
+    const off = document.querySelector("img").src = "./img/pic_bulboff.gif";
+    if (imagem == off){
+        document.querySelector("img").src = "./img/pic_bulboff.gif";
+        btnOnOff.replaceChildren("Ligar");
+    }else{
+        document.querySelector("img").src = "./img/pic_bulbon.gif";          
+        btnOnOff.replaceChildren("Desligar");
+    }
     
-    btnOn.addEventListener('click', function(){
-        document.querySelector("img").src = "./img/pic_bulbon.gif"
-        btnOn.replaceChildren("Desliga")
-        
 
-    })  
-
-    btnOff.addEventListener('click', function(){
-        document.querySelector("img").src = "./img/pic_bulboff.gif"        
-    })
-
-
-
-
-
+    // btnOff.addEventListener('click', function(){
+    //     document.querySelector("img").src = "./img/pic_bulboff.gif"        
+    // })
 }
-    
-
+document.querySelector(".ButtonLiga").addEventListener('click', LigaDesliga);
 
     //NORMALIZE ESSE EXERCÍCIO
     //TRANSFORME ESTAS LINHAS EM APENAS UMA FUNÇÃO!
