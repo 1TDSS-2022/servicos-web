da151bad2dc68b2285b08be6f9cce6b5ee5eb7de
HenriqueCesarS <rm94276@fiap.com.br>
2022-05-13 21:03:34 -0300

    function, const add

--- JS/scpitlampada.js
+++ JS/scpitlampada.js
@@ -1,15 +1,32 @@
 
-    const btnOn = document.querySelector("#ligar")
+    //const btnOn = document.querySelector("#ligar")
 
-    btnOn.addEventListener('click', function() {
-        document.querySelector("img").src = "./img/pic_bulbon.gif"
-    })
+   // btnOn.addEventListener('click', function() {
+   //     document.querySelector("img").src = "./img/pic_bulbon.gif"
+  //  })
 
-    const btnOff = document.querySelector("#desligar")
+   // const btnOff = document.querySelector("#desligar")
 
-    btnOff.addEventListener('click', function() {
-        document.querySelector("img").src = "./img/pic_bulboff.gif"
+   // btnOff.addEventListener('click', function() {
+   //     document.querySelector("img").src = "./img/pic_bulboff.gif"
 
-    })
+   // })
+    
+ //   const divElemento = document.querySelector(".box")
+  ///  divElemento.innerHTML = "Valor que eu quero"
+   
+// Funções Matemáticas
+
+
+//function conta(){
+//    let valor = document.querySelector(".box").innerHTML  
+//    console.log("Valor de inner:", valor)
+//}
+
+// ou 
+
+//function conta2(parametro){
+  // console.log("Parâmetro passado: " + parametro)
+// }
 
 
